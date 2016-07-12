7047f6a4e810c46b666beb97cc3c2ca3f978806e
sebabelmar <seba.belmar@devbootcamp.com>
2016-07-12 10:58:36 -0700

    Added type animation library and added style to it

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -4,5 +4,11 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
   function ($scope, Authentication) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
+
+    $scope.message = ["Add a memory...",
+      "Add memories to your Instagram.",
+      "Keep it private, and share it with your closest circle.",
+      "Invite people to add memories.",
+      ""];
   }
 ]);
